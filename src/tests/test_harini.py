c74c850a4044df892136fa4db59b7692d4d12416
Jon Gartman <jgartman@pulsesecure.net>
2021-05-10 14:55:51 -0500

    Fixing up some tests, integrating POC into polydatum

--- src/tests/test_harini.py
+++ src/tests/test_harini.py
@@ -1,48 +1,18 @@
+import pytest
 from polydatum import DataAccessLayer, DataManager, Service
-from polydatum.middleware import DataAccessLayer as MiddlewareDataAccessLayer
 
-
-def test_middleware_dal():
-    """
-    Verify middleware DataAccessLayer can be passed to DataManager
-    """
-
-    class Example(DataManager):
-        """
-        DataManager wrapper for test
-        """
-        DataAccessLayer = MiddlewareDataAccessLayer
-
-        def __init__(self, resource_manager=None):
-            super(Example, self).__init__(resource_manager)
-
-    # Verify self._dal is set to the default DataAccessLayer class
-    dm = DataManager()
-    assert isinstance(dm._dal, DataAccessLayer)
-
-    # Verify self._dal is set to the optional DataAccessLayer class i.e. TestDal class
-    dm = Example()
-    assert isinstance(dm._dal, MiddlewareDataAccessLayer)
 
 
 def test_service_call():
     """
     Verify dal method calls
     """
-    class Example(DataManager):
-        """
-        DataManager wrapper for test
-        """
-        DataAccessLayer = MiddlewareDataAccessLayer
-
-        def __init__(self, resource_manager=None):
-            super(Example, self).__init__(resource_manager)
+    dm = DataManager()
 
     class ExampleService(Service):
         def get_user(self):
-            return self._ctx.meta.user
+            return True
 
-    dm = Example()
     dm.register_services(test=ExampleService())
 
     with dm.context() as ctx:
